f1f5ccfa2b71f26bf371db149f991724fdea83ce
Graham Cooper <graham.cooper@shopify.com>
2025-11-19 16:27:16 -0500

    fix

--- scripts/generate_fingerprint_outfuncs.rb
+++ scripts/generate_fingerprint_outfuncs.rb
@@ -258,7 +258,7 @@ class Generator
     ['RawStmt', 'stmt_location'] => :skip,
   }
   INT_TYPES = ['bits32', 'uint32', 'int', 'int32', 'uint16', 'int16', 'Oid', 'Index', 'AclMode', 'AttrNumber', 'SubTransactionId']
-  LONG_INT_TYPES = ['long', 'uint64', 'uint64_t']
+  LONG_INT_TYPES = ['long', 'uint64']
   INT_ARRAY_TYPES = ['Bitmapset*', 'Bitmapset', 'Relids']
   FLOAT_TYPES = ['Cost', 'double', 'Cardinality']
 
