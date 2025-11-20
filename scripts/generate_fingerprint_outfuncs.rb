c0c06b7ab25c9c49aea87d53375f7908c6b36d1f
Graham Cooper <graham.cooper@shopify.com>
2025-11-20 10:35:15 -0500

    Remove prffailedloginattempts

--- scripts/generate_fingerprint_outfuncs.rb
+++ scripts/generate_fingerprint_outfuncs.rb
@@ -257,6 +257,7 @@ class Generator
     ['RawStmt', 'stmt_len'] => :skip,
     ['RawStmt', 'stmt_location'] => :skip,
     ['YbBackfillInfo', 'read_time'] => :skip,
+    ['YbCreateProfileStmt', 'prffailedloginattempts'] => :skip,
   }
   INT_TYPES = ['bits32', 'uint32', 'int', 'int32', 'uint16', 'int16', 'Oid', 'Index', 'AclMode', 'AttrNumber', 'SubTransactionId']
   LONG_INT_TYPES = ['long', 'uint64']
